131c855c64c5d28466a4d09bf543e00bdaf50220
Akash Jadhav <102428182+jadhavakashofficial@users.noreply.github.com>
2025-03-10 20:19:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//your code here
+const motivationButton = document.getElementById('motivationButton');
+motivationButton.addEventListener('click', function() {
+  alert('Keep going! You are on the path to becoming an awesome developer!');
+});
